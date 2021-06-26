6633a2eae836d37a8c05d039b92cda2c4bd41671
awlan04 <cawlana040@gmail.com>
2021-06-25 22:21:20 -0300

    graphql

---
+++ lib/wabanex_web/schemas/types/root.ex
@@ -0,0 +1,16 @@
+defmodule WabanexWeb.Schemas.Types.Root do
+  use Absinthe.Schema.Notation
+
+  alias WabanexWeb.Resolver.User, as: UserResolver
+
+  import_types WabanexWeb.Schemas.Types.User
+
+  @desc "Login root representation"
+  object :root_query do
+    field :get_user, type: :user do
+      arg :id, non_null(:uuid4)
+
+      resolve &UserResolver.get/2
+    end
+  end
+end
